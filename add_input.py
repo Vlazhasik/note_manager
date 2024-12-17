e2c8047ecf76ed8d16a17cff173d4b5a9cc18d5a
Vlazhasik <154734711+Vlazhasik@users.noreply.github.com>
2024-12-17 11:50:19 +0700

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,6 @@
+username = input('Введите имя пользователя: ')
+title = input('Введите заголовок: ')
+content = input('Укажите заметку: ')
+status = 'Не выполненно'
+created_date = int(input('Укажите дату выполнения заметки: '))
+issue_date = int(input('Укажите крайнюю дату заметки: '))
