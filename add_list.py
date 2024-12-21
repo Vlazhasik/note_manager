6c3a95ed67dc111eb20bd36ff3f7d0f1aac449cf
Vlazhasik <154734711+Vlazhasik@users.noreply.github.com>
2024-12-21 12:52:19 +0700

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,11 @@
+quest_name=[]
+important=[]
+description=[]
+
+quest_name=input('Укажите заголовок заметки: ')
+important=input('Укажите приоритет заметки: ')
+description=input('Укажите краткое описание заметки: ')
+
+print(quest_name)
+print(important)
+print(description)
