1c4f98f6a2b5d3582a23cd3acb0831c499e6bebc
Vlazhasik <154734711+Vlazhasik@users.noreply.github.com>
2024-12-24 14:41:30 +0700

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -1,11 +1,11 @@
-quest_name=[]
-important=[]
-description=[]
+main_list=[]
 
 quest_name=input('Укажите заголовок заметки: ')
 important=input('Укажите приоритет заметки: ')
 description=input('Укажите краткое описание заметки: ')
 
-print(quest_name)
-print(important)
-print(description)
+main_list.append(quest_name)
+main_list.append(important)
+main_list.append(description)
+
+print(main_list)
